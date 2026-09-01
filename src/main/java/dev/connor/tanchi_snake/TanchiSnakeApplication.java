525d9df5101e116e6b2adb85c2f1ce572804f43f
Connor Chen <epicconnor1229@gmail.com>
2026-09-01 18:49:29 -0400

    Spring Boot skeleton: web + websocket

---
+++ src/main/java/dev/connor/tanchi_snake/TanchiSnakeApplication.java
@@ -0,0 +1,13 @@
+package dev.connor.tanchi_snake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanchiSnakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanchiSnakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/connor/tanchi_snake/TanchiSnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.connor.tanchi_snake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanchiSnakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
